7fbcbab7308c852ed235182a0ae331a893873fae
laurenlietzke <116765510+laurenlietzke@users.noreply.github.com>
2022-11-08 15:36:11 -0500

    Add files via upload

---
+++ Products for Users - Phase 2.py
@@ -0,0 +1,33 @@
+'''
+Project Phase 2
+Warehouse Managment Systems
+Team ABC
+
+Author: Lauren Lietzke
+Page: Products for Users
+'''
+
+import tkinter as tk
+import tkmacosx as tkmac
+window=tk.Tk()
+window.title('Warehouse Management System')
+window.geometry('800x500')
+window.configure(bg='blue')
+
+lblUser_products=tk.Label(window,text='User Product Window', bg='blue', fg='white', padx=100)
+lblUser_products.grid(row=0,column=0)
+
+texProducts=tk.Text(window, bg='white', height=25, width=50, padx=50)
+texProducts.grid(row=1,column=0)
+
+btnAdd=tkmac.Button(window, text='Add to Favorites List', padx=30)
+btnAdd.grid(row=0, column=1)
+
+btnRequest=tkmac.Button(window, text='Request to Borrow', padx=30)
+btnRequest.grid(row=1, column=1)
+
+btnSearch=tkmac.Button(window, text='Search Product Library', padx=30)
+btnSearch.grid(row=3, column=1)
+
+window.mainloop()
+
