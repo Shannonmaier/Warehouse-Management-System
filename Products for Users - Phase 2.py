ef6f4cd7e189d408117315c0cfde0f6a9254f952
laurenlietzke <116765510+laurenlietzke@users.noreply.github.com>
2022-11-08 16:07:35 -0500

    Add files via upload

--- Products for Users - Phase 2.py
+++ Products for Users - Phase 2.py
@@ -20,14 +20,17 @@ lblUser_products.grid(row=0,column=0)
 texProducts=tk.Text(window, bg='white', height=25, width=50, padx=50)
 texProducts.grid(row=1,column=0)
 
-btnAdd=tkmac.Button(window, text='Add to Favorites List', padx=30)
+btnAdd=tkmac.Button(window, text='Add Product to Favorites List', padx=30)
 btnAdd.grid(row=0, column=1)
 
-btnRequest=tkmac.Button(window, text='Request to Borrow', padx=30)
+btnRequest=tkmac.Button(window, text='Request to Borrow a Product', padx=30)
 btnRequest.grid(row=1, column=1)
 
 btnSearch=tkmac.Button(window, text='Search Product Library', padx=30)
-btnSearch.grid(row=3, column=1)
+btnSearch.grid(row=2, column=1)
+
+btnReturn=tkmac.Button(window, text='Return to Main Menu', padx=30)
+btnReturn.grid(row=3, column=1)
 
 window.mainloop()
 
